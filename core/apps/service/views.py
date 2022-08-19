d32063b5d739fb4cb30320a9e58e07b7910334ed
unknown <juanignaciodelossantos01@gmail.com>
2022-08-19 11:55:49 -0300

    ruta y vista de app service creada

--- core/apps/service/views.py
+++ core/apps/service/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIview
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
 
-# Create your views here.
+from .serializers import ServiceSerializers
+
+from .models import Service
+
+class ListServicesView(APIview):
+    permissions_classes = (permissions.AllowAny,)
+
+    def get(self, request, format=None):
+        if Service.objects.all().exists():
+            services = Service.objects.all()
+            services = ServiceSerializers(services)
+            return Response({'services': services.data}, status=status.HTTP_200_OK)
+        else: 
+            return Response({'error': 'No services found'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
